f5ca072d2fee8debb11a7de9b405e150a5d583fb
Charlie Cook <turingbombe@gmail.com>
2016-08-05 14:57:27 -0400

    Complted mapping to table lab team: @marya314, @colbyschulz, @daniellasheva

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,47 @@
 class Student
-
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+  def initialize(id=nil, name, grade) 
+    @name = name
+    @grade = grade
+    @id = id  
+  end  
+
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade TEXT);
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade) VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    sql = <<-SQL
+    SELECT last_insert_rowid() FROM students
+    SQL
+    @id = DB[:conn].execute(sql).first.first
+    self 
+  end
+
+  def self.create(name:, grade:)
+    Student.new(name,grade).save
+  end
+
   
 end
